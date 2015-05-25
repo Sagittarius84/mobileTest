57d1e1a5073864c18d90de6b1f891de9cae09b5d
Michael Wodniok <michi@noorganization.org>
2015-05-25 18:23:03 +0200

    Added tests and implementation for CategoryController.

--- app/src/androidTest/java/org/noorganization/instalist/controller/ICategoryControllerTest.java
+++ app/src/androidTest/java/org/noorganization/instalist/controller/ICategoryControllerTest.java
@@ -4,6 +4,7 @@ import android.test.AndroidTestCase;
 
 import com.orm.SugarRecord;
 
+import org.noorganization.instalist.controller.implementation.ControllerFactory;
 import org.noorganization.instalist.model.Category;
 import org.noorganization.instalist.model.ShoppingList;
 
@@ -19,7 +20,10 @@ public class ICategoryControllerTest extends AndroidTestCase {
         mCategoryWork = new Category("_TEST_work");
         mCategoryWork.save();
         mListHardwareStore = new ShoppingList("_TEST_hardware store", mCategoryWork);
+        mListHardwareStore.save();
         mCategoryWork.save();
+
+        mCategoryController = ControllerFactory.getCategoryController();
     }
 
     public void tearDown() throws Exception {
@@ -31,17 +35,39 @@ public class ICategoryControllerTest extends AndroidTestCase {
     }
 
     public void testCreateCategory() throws Exception {
+        assertNull(mCategoryController.createCategory(null));
+        assertNull(mCategoryController.createCategory(""));
+        assertNull(mCategoryController.createCategory("_TEST_work"));
 
-        fail("This test is a stub.");
+        Category returnedCreatedCategory1 = mCategoryController.createCategory("_TEST_home");
+        assertNotNull(returnedCreatedCategory1);
+        assertEquals("_TEST_home", returnedCreatedCategory1.mName);
+        assertEquals(returnedCreatedCategory1,
+                SugarRecord.findById(Category.class, returnedCreatedCategory1.getId()));
     }
 
     public void testRenameCategory() throws Exception {
+        assertNull(mCategoryController.renameCategory(null, ""));
+        Category notSavedCategory = new Category("_TEST_not saved category");
+        assertNull(mCategoryController.renameCategory(notSavedCategory, "_TEST_still not saved category"));
+        assertEquals(mCategoryWork, mCategoryController.renameCategory(mCategoryWork, ""));
 
-        fail("This test is a stub.");
+        Category returnedRenamedCategory1 = mCategoryController.renameCategory(mCategoryWork, "_TEST_home");
+        assertNotNull(returnedRenamedCategory1);
+        assertEquals("_TEST_home", returnedRenamedCategory1.mName);
+        assertEquals(returnedRenamedCategory1, SugarRecord.findById(Category.class, mCategoryWork.getId()));
     }
 
     public void testRemoveCategory() throws Exception {
+        // Nothing should happen if a wrong input is given.
+        mCategoryController.removeCategory(null);
+        Category notSavedCategory = new Category("_TEST_not saved category");
+        mCategoryController.removeCategory(notSavedCategory);
 
-        fail("This test is a stub.");
+        // If a category gets deleted, lists have to be unlinked.
+        long deletedId = mCategoryWork.getId();
+        mCategoryController.removeCategory(mCategoryWork);
+        assertNull(SugarRecord.findById(Category.class, deletedId));
+        assertNull(SugarRecord.findById(ShoppingList.class, mListHardwareStore.getId()).mCategory);
     }
 }
--- app/src/main/java/org/noorganization/instalist/controller/implementation/CategoryController.java
+++ app/src/main/java/org/noorganization/instalist/controller/implementation/CategoryController.java
@@ -1,7 +1,14 @@
 package org.noorganization.instalist.controller.implementation;
 
+import com.orm.SugarRecord;
+import com.orm.query.Condition;
+import com.orm.query.Select;
+
 import org.noorganization.instalist.controller.ICategoryController;
 import org.noorganization.instalist.model.Category;
+import org.noorganization.instalist.model.ShoppingList;
+
+import java.util.List;
 
 public class CategoryController implements ICategoryController {
     private static CategoryController mInstance;
@@ -18,16 +25,61 @@ public class CategoryController implements ICategoryController {
 
     @Override
     public Category createCategory(String _name) {
-        return null;
+        if (_name == null || _name.length() == 0 || nameUsed(_name, null)) {
+            return null;
+        }
+
+        Category rtn = new Category(_name);
+        rtn.save();
+
+        return rtn;
     }
 
     @Override
     public Category renameCategory(Category _toRename, String _newName) {
-        return null;
+        if (_toRename == null) {
+            return null;
+        }
+
+        Category rtn = SugarRecord.findById(Category.class, _toRename.getId());
+        if (rtn == null) {
+            return null;
+        }
+
+        if (_newName != null && _newName.length() > 0 && !nameUsed(_newName, rtn.getId())) {
+            rtn.mName = _newName;
+            rtn.save();
+        }
+
+        return rtn;
     }
 
     @Override
     public void removeCategory(Category _toRemove) {
+        if (_toRemove == null) {
+            return;
+        }
 
+        if (SugarRecord.findById(Category.class, _toRemove.getId()) != null) {
+            List<ShoppingList> listsToUnlink = Select.from(ShoppingList.class).
+                    where(Condition.prop(ShoppingList.ATTR_CATEGORY).eq(_toRemove.getId())).list();
+            for (ShoppingList currentList : listsToUnlink) {
+                currentList.mCategory = null;
+            }
+            SugarRecord.saveInTx(listsToUnlink);
+
+            _toRemove.delete();
+        }
+    }
+
+    private boolean nameUsed(String _search, Long _ignoreId) {
+        for (Category toCheck : Select.from(Category.class).
+                where(Condition.prop(Category.ATTR_NAME).eq(_search)).list()) {
+            if (_ignoreId == null || _ignoreId.compareTo(toCheck.getId()) != 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
