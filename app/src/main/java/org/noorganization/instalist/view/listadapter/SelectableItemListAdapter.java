813b26d8031b675679eafb193ac37d45ce6c8650
Michael Wodniok <michi@noorganization.org>
2015-07-20 23:06:56 +0200

    Added menu for editing and removing Products/Recipes.
    Added also view-logic for deletion as far as currently possible.

--- app/src/main/java/org/noorganization/instalist/view/listadapter/SelectableItemListAdapter.java
+++ app/src/main/java/org/noorganization/instalist/view/listadapter/SelectableItemListAdapter.java
@@ -1,19 +1,29 @@
 package org.noorganization.instalist.view.listadapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.orm.SugarRecord;
 
 import org.noorganization.instalist.R;
+import org.noorganization.instalist.controller.IProductController;
+import org.noorganization.instalist.controller.IRecipeController;
+import org.noorganization.instalist.controller.implementation.ControllerFactory;
+import org.noorganization.instalist.model.Product;
+import org.noorganization.instalist.model.Recipe;
 import org.noorganization.instalist.view.activity.RecipeChangeActivity;
 import org.noorganization.instalist.view.dataholder.SelectableBaseItemListEntryDataHolder;
 import org.noorganization.instalist.view.fragment.ProductChangeFragment;
@@ -60,7 +70,7 @@ public class SelectableItemListAdapter extends ArrayAdapter<SelectableBaseItemLi
 
     @Override
     public View getView(int _Position, View _ConvertView, ViewGroup _Parent) {
-        View view = null;
+        View view;
         SelectableBaseItemListEntry listEntry      = mSelectableItems.get(_Position);
 
         if(_ConvertView == null){
@@ -133,23 +143,114 @@ public class SelectableItemListAdapter extends ArrayAdapter<SelectableBaseItemLi
 
         @Override
         public boolean onLongClick(View v) {
-            SugarRecord currentEntry = (SugarRecord) (mListEntry.getEntry().getObject());
+            PopupMenu actionMenu = new PopupMenu(getContext(), v);
+            actionMenu.inflate(R.menu.menu_productrecipe_action_popup);
+            actionMenu.setOnMenuItemClickListener(new OnListEntryPopupMenuClickListener(mListEntry));
+            actionMenu.show();
+            return true;
+        }
+    }
+
+    private class OnListEntryPopupMenuClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        private IBaseListEntry mListEntry;
+
+        public OnListEntryPopupMenuClickListener(IBaseListEntry _listEntry) {
+            super();
+            mListEntry = _listEntry;
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            SugarRecord item = ((SugarRecord) mListEntry.getEntry().getObject());
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    switch (mListEntry.getType()) {
+                        case PRODUCT_LIST_ENTRY:
+                            ViewUtils.addFragment(mActivity, ProductChangeFragment.
+                                    newChangeInstance(item.getId()));
+                            break;
+                        case RECIPE_LIST_ENTRY:
+                            Intent startEditor = new Intent(getContext(), RecipeChangeActivity.class);
+                            startEditor.putExtra(RecipeChangeActivity.ARGS_RECIPE_ID, item.getId());
+                            getContext().startActivity(startEditor);
+                            break;
+                        default:
+                            throw new IllegalStateException("There is no entry type defined.");
+                    }
+                    break;
+                case R.id.action_delete:
+                    onDelete();
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+
+        private void onDelete() {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
+            dialogBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
 
             switch (mListEntry.getType()) {
                 case PRODUCT_LIST_ENTRY:
-                    ViewUtils.addFragment(mActivity, ProductChangeFragment.
-                            newChangeInstance(currentEntry.getId()));
+                    dialogBuilder.setMessage(getContext().
+                            getString(R.string.remove_product_question, mListEntry.getName()));
+                    dialogBuilder.setPositiveButton(android.R.string.yes,
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            IProductController productController = ControllerFactory.getProductController();
+                            if (productController.removeProduct(
+                                    (Product) mListEntry.getEntry().getObject(), false)) {
+                                Toast.makeText(getContext(), R.string.removed_product, Toast.LENGTH_SHORT).
+                                        show();
+                            } else {
+                                AlertDialog.Builder secondaryBuilder = new AlertDialog.Builder(getContext());
+                                DialogInterface.OnClickListener clickListener =
+                                        new SecondaryProductActionClickListener();
+                                secondaryBuilder.setPositiveButton(android.R.string.yes, clickListener);
+                                secondaryBuilder.setNegativeButton(android.R.string.no, clickListener);
+                                secondaryBuilder.setMessage(R.string.remove_product_question2);
+                                secondaryBuilder.show();
+                            }
+                        }
+                    });
                     break;
                 case RECIPE_LIST_ENTRY:
-                    Intent startEditor = new Intent(getContext(), RecipeChangeActivity.class);
-                    startEditor.putExtra(RecipeChangeActivity.ARGS_RECIPE_ID, currentEntry.getId());
-                    getContext().startActivity(startEditor);
+                    dialogBuilder.setMessage(getContext().
+                            getString(R.string.remove_recipe_question, mListEntry.getName()));
+                    dialogBuilder.setPositiveButton(android.R.string.yes,
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            IRecipeController recipeController = ControllerFactory.getRecipeController();
+                            recipeController.removeRecipe((Recipe) mListEntry.getEntry().getObject());
+                            Toast.makeText(getContext(), R.string.removed_recipe, Toast.LENGTH_SHORT).
+                                    show();
+                        }
+                    });
                     break;
-                default:
-                    throw new IllegalStateException("There is no entry type defined.");
             }
 
-            return true;
+            dialogBuilder.show();
+        }
+
+        private class SecondaryProductActionClickListener implements DialogInterface.OnClickListener {
+
+            @Override
+            public void onClick(DialogInterface _dialogInterface, int _whichButton) {
+                if (_whichButton == DialogInterface.BUTTON_POSITIVE) {
+                    IProductController productController = ControllerFactory.getProductController();
+                    productController.removeProduct((Product) mListEntry.getEntry().getObject(), true);
+                    Toast.makeText(getContext(), R.string.removed_product, Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     }
 
@@ -168,7 +269,7 @@ public class SelectableItemListAdapter extends ArrayAdapter<SelectableBaseItemLi
                     result.count = listEntries.size();
                 } else {
                     IBaseListEntry.eItemType filterType;
-                    ArrayList<SelectableBaseItemListEntry> filteredList = new ArrayList<SelectableBaseItemListEntry>();
+                    ArrayList<SelectableBaseItemListEntry> filteredList = new ArrayList<>();
 
                     switch (constraint.toString()) {
                         case ProductListDialogFragment.FILTER_BY_PRODUCT:
@@ -182,7 +283,7 @@ public class SelectableItemListAdapter extends ArrayAdapter<SelectableBaseItemLi
                             break;
                         default:
                             filterType = IBaseListEntry.eItemType.NAME_SEARCH;
-                            ;
+
                             String contraintToFind = constraint.toString().toLowerCase();
                             for (SelectableBaseItemListEntry entry : listEntries) {
                                 if (entry.getItemListEntry().getName().toLowerCase().startsWith(contraintToFind)) {
@@ -215,5 +316,4 @@ public class SelectableItemListAdapter extends ArrayAdapter<SelectableBaseItemLi
         };
         return filter;
     }
-
 }
