246ce6cedda68ee082bd871ba8ac43b44785f396
Michael Wodniok <michi@noorganization.org>
2016-01-07 09:22:39 +0100

    Added some Plugin-Code.

--- app/src/main/java/org/noorganization/instalist/presenter/event/PluginFoundMessage.java
+++ app/src/main/java/org/noorganization/instalist/presenter/event/PluginFoundMessage.java
@@ -1,17 +1,28 @@
 package org.noorganization.instalist.presenter.event;
 
+import android.app.Activity;
+
 /**
  * Event-Message for notifying about found plugins.
  * Created by damihe on 06.01.16.
  */
 public class PluginFoundMessage {
     public String mName;
-    public String mSettingsActivity;
     public String mPackage;
+    public Class<?> mMainActivity;
+    public Class<?> mSettingsActivity;
 
-    public PluginFoundMessage(String _name, String _settingsActivity, String _package) {
+
+    public PluginFoundMessage(String _name, Class<Activity> _mainActivity,
+                              Class<Activity> _settingsActivity, String _package) {
         mName = _name;
+        mMainActivity = _mainActivity;
         mSettingsActivity = _settingsActivity;
+        mPackage = _package;
+    }
+
+    public PluginFoundMessage(String _name, String _package) {
+        mName = _name;
         mPackage = _package;
     }
 }
--- app/src/main/java/org/noorganization/instalist/presenter/implementation/PluginController.java
+++ app/src/main/java/org/noorganization/instalist/presenter/implementation/PluginController.java
@@ -1,16 +1,24 @@
 package org.noorganization.instalist.presenter.implementation;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import org.noorganization.instalist.presenter.IPluginController;
+import org.noorganization.instalist.presenter.event.PluginFoundMessage;
 
 /**
  * Created by damihe on 06.01.16.
  */
 public class PluginController extends BroadcastReceiver implements IPluginController {
+
+    static final String PLUGIN_INFO_KEY_NAME = "name";
+    static final String PLUGIN_INFO_KEY_SETTINGS_ACTIVITY = "settings";
+    static final String PLUGIN_INFO_KEY_MAIN_ACTIVITY = "main";
+    static final String PLUGIN_INFO_KEY_PACKAGE = "package";
 
     public static final String ACTION_PING = "org.noorganization.instalist.action.PING_PLUGIN";
     public static final String ACTION_PONG = "org.noorganization.instalist.action.PONG_PLUGIN";
@@ -23,11 +31,36 @@ public class PluginController extends BroadcastReceiver implements IPluginContro
         Log.d("BroadcastSender", "Sending " + ACTION_PING);
         Intent pingBroadcast = new Intent(ACTION_PING);
         pingBroadcast.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        pingBroadcast.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.sendBroadcast(pingBroadcast);
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context _context, Intent _intent) {
+        Log.d(getClass().getCanonicalName(), "Got Intent.");
+        Bundle pluginInfo = _intent.getExtras();
+        PluginFoundMessage event = new PluginFoundMessage(pluginInfo.getString(PLUGIN_INFO_KEY_NAME),
+                pluginInfo.getString(PLUGIN_INFO_KEY_PACKAGE));
+        Log.d(getClass().getCanonicalName(), "Trying find main activity");
+        if (pluginInfo.containsKey(PLUGIN_INFO_KEY_MAIN_ACTIVITY)) {
+            try {
+                event.mMainActivity = Class.forName(
+                        pluginInfo.getString(PLUGIN_INFO_KEY_MAIN_ACTIVITY));
+            } catch (Exception e) {
+                Log.e(getClass().getCanonicalName(), "Plugin loading failed: " + e.getMessage());
+                return;
+            }
+        }
+        Log.d(getClass().getCanonicalName(), "Trying find settings activity");
+        if (pluginInfo.containsKey(PLUGIN_INFO_KEY_SETTINGS_ACTIVITY)) {
+            try {
+                event.mSettingsActivity = Class.forName(
+                        pluginInfo.getString(PLUGIN_INFO_KEY_SETTINGS_ACTIVITY));
+            } catch (Exception e) {
+                Log.e(getClass().getCanonicalName(), "Plugin loading failed: " + e.getMessage());
+            }
+        }
+        Log.d(getClass().getCanonicalName(), "Found Plugin! " + event.mName + " in " + event.mPackage);
         // TODO: implement translator to event for bus.
     }
 
